aae2ca93a5f1ebc38c1ff63e5cccb55a2376b43c
justin <justinphil02@yahoo.com>
2020-03-22 12:40:10 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,10 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)     
+    Student.where("name LIKE ?", "%#{query}%")
+    #User.where(["name = ? and email = ?", "Joe", "joe@example.com"])
+    #Client.where("first_name LIKE '%#{params[:first_name]}%'") is not safe. 
+  end 
 end
